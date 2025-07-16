4549d3e89fd8e7a61329580967c3cdf3f2bdcd2b
Dian Leithon Isidro <dlv.isidro@gmail.com>
2025-07-16 23:58:05 +0800

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -82,59 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  // void _directPrintPdf(Printer? printer, List<Person> peopleToPrint) async {
-  //   if (printer == null) {
-  //     ScaffoldMessenger.of(
-  //       context,
-  //     ).showSnackBar(const SnackBar(content: Text('No printer selected.')));
-  //     return;
-  //   }
-  //   final doc = await generatePdf(context, peopleToPrint);
-  //   if (doc == null) return;
-
-  //   await Printing.directPrintPdf(
-  //     printer: printer,
-  //     onLayout: (PdfPageFormat format) async => doc.save(),
-  //   );
-  // }
-
-  // Future<pw.Document?> _generatePdf(List<Person> peopleToPrint) async {
-  //   if (peopleToPrint.isEmpty) {
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(content: Text('No people selected to print.')),
-  //     );
-  //     return null;
-  //   }
-
-  //   final doc = pw.Document();
-  //   const pageFormat = PdfPageFormat(
-  //     70 * PdfPageFormat.mm,
-  //     50 * PdfPageFormat.mm,
-  //   );
-  //   // const pageFormat = PdfPageFormat.a4;
-
-  //   for (var person in peopleToPrint) {
-  //     doc.addPage(
-  //       pw.Page(
-  //         pageFormat: pageFormat,
-  //         build: (pw.Context context) {
-  //           return pw.Center(
-  //             child: pw.Column(
-  //               mainAxisAlignment: pw.MainAxisAlignment.center,
-  //               children: [
-  //                 pw.Text(person.firstName),
-  //                 pw.Text(person.lastName),
-  //                 pw.Text(person.churchName),
-  //               ],
-  //             ),
-  //           ); // Center
-  //         },
-  //       ),
-  //     ); // Page
-  //   }
-  //   return doc;
-  // }
-
   void _showPrintPreview(pw.Document doc) {
     showDialog(
       context: context,
